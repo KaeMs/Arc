915016ff4fa87e08c465e994c5a0a0954b25969b
Kevin <kaemsss@gmail.com>
2017-04-16 10:09:14 +0700

    Added forgot password xml

---
+++ app/src/main/java/com/med/fast/ForgotPassActivity.java
@@ -0,0 +1,16 @@
+package com.med.fast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Kevin on 4/16/2017. FM
+ */
+
+public class ForgotPassActivity extends FastBaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
